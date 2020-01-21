8cb87b6be69b79d37577195ed8c1da619a39c7f5
fredlabbe <labbef.hockey@gmail.com>
2020-01-21 16:39:03 -0500

    Act1: Finished activity1

--- Activities/Activity2jQuery/js/script.js
+++ Activities/Activity2jQuery/js/script.js
@@ -9,3 +9,51 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+let $spans;
+const intervalValue = 500;
+let probability = 0.1;
+$(document).ready(setup);
+
+// function setup(){
+//   console.log("loaded!");
+//   //prefix jQuery vars w/ $ to id jQuery
+//   // let $divs = $('div');//can put .game-icon or any other css class
+//   // $divs.hide();
+//   // $divs.fadeIn(2000);
+//   //$('div').hide().fadeIn(2000); // Select all divs THEN hide them THEN fade them in
+//
+// }
+
+function setup(){
+  setInterval(update,intervalValue);
+  $spans = $('span')
+  $spans.on("click",spanClicked);
+}
+//update()
+//
+//
+function update(){
+  console.log("update!");
+  $spans.each(updateSpan);
+}
+//updateSpan()
+//
+//
+function updateSpan(){
+  console.log("update span!");
+  let r = Math.random();
+  if (r < probability){
+    $(this).removeClass("redacted");
+    $(this).addClass("revealed");
+  }
+}
+//spanClicked()
+//
+//
+function spanClicked(){
+  console.log("span clicked");
+  $(this).removeClass("revealed");
+  $(this).addClass("redacted");
+
+
+}
--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -47,21 +47,20 @@ function remove(e){
 e.target.style.opacity = "0";
 }
 function rotate(e){
+  let pixels = document.querySelectorAll('.pixel');
   //left arrow down to rotate left by 1px
   if(e.keyCode === 37){
-    let pixels = document.querySelectorAll('.pixel');
-
-// GO SEE WHAT HE DID IN CLASS TO GET THE VALUE AND PARSEFLOAT
-
+    rotation -= 1;
     for(let i = 0; i < pixels.length; i++){
-      //let
-      //let currentAngle = document.
-      //pixels[i].style.transform
+      pixels[i].style.transform = `rotate(${rotation}deg)`;
     }
   }
   //right arrow down to rotate right by 1px
   if(e.keyCode === 39){
-
+    rotation += 1;
+    for(let i = 0; i < pixels.length; i++){
+      pixels[i].style.transform = `rotate(${rotation}deg)`;
+    }
   }
 
 }
