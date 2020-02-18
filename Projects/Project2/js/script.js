3afcd9cafdac02d4c8f265e4a9ba4096c5a82fdc
fredlabbe <labbef.hockey@gmail.com>
2020-02-18 17:32:40 -0500

    P2: Player can now bounce on the plateform but error with the cursors

--- Projects/Project2/js/script.js
+++ Projects/Project2/js/script.js
@@ -14,6 +14,7 @@ author, and this description to match your project!
 // in the object.
 let player
 let platforms;
+let cursors;
 var config = {
   // The renderer to be used: CANVAS, WEBGL, AUTO)
   type: Phaser.AUTO,
@@ -60,10 +61,12 @@ function preload() {
 // Sets up the game
 
 function create() {
+  cursors = this.input.keyboard.createCursorKeys();
+
   platforms = this.physics.add.staticGroup();
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
   player = this.physics.add.sprite(100, 150, 'character');
-  player.setBounce(1);
+  player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
   this.physics.add.collider(player, platforms);
@@ -77,6 +80,35 @@ function create() {
 //
 
 function update() {
+  if (cursors.left.isDown) {
+      // setVelocityX will start the player moving at that number of pixels per second
+      // We don't need to do anything more than this
+      player.setVelocityX(-160);
+      // We play an animation using the sprite's anims property and giving it the appropriate animation key
+      player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+      // Similarly for right
+      player.setVelocityX(160);
+      player.anims.play('right', true);
+    }
+    else {
+      // If neither left nor right is pressed the player should stop
+      // so we set its velocity to 0 and turn it to face the front
+      player.setVelocityX(0);
+      player.anims.play('turn');
+    }
 
+    // Slightly more fancy for jumping
+    // We check if the up key is down
+    // And we also check on the player's 'body' property (its physics body)
+    // whether it is touching something in the dowards direction.
+    // This means it will only jump if it is standing on something. Nice!
+    if (cursors.up.isDown && player.body.touching.down) {
+      // Jumping just means setting an upward velocity.
+      // Remember we set up gravity right at the beginning, so that will cause
+      // the player to fall appropriately.
+      player.setVelocityY(-330);
+    }
 
 }
